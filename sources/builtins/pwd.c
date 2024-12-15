f6f53b3b171e00935eeb04f6cf8340c666fb2f4e
rda-cunh <ricard.mendes@gmail.com>
2024-12-15 00:29:11 +0000

    updated pwd

--- sources/builtins/pwd.c
+++ sources/builtins/pwd.c
@@ -3,11 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lmaes <lmaes@student.42porto.com>          +#+  +:+       +#+        */
+/*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:03:45 by lmaes             #+#    #+#             */
-/*   Updated: 2024/11/25 19:03:46 by lmaes            ###   ########.fr       */
+/*   Updated: 2024/12/15 00:28:28 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+int pwd(void)
+{
+    char    *cwd;
+
+    cwd = getcwd(NULL, 0);
+    if (!cwd)
+    {
+        perror("pwd error");
+        return (1);
+    }
+    printf("%s\n", cwd);
+    free(cwd);
+    return (0); 
+}
+/*
+int main()
+{
+    pwd();
+    return 0;
+}
+/*
+
