67ad196ca95b6ef492f7f501772c66999b69df1a
rda-cunh <ricard.mendes@gmail.com>
2025-02-13 02:15:36 +0000

    norm corrections on  tokenizer.c

--- sources/Parser_and_tokens/tokenizer.c
+++ sources/Parser_and_tokens/tokenizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lmaes <lmaes@student.42porto.com>          +#+  +:+       +#+        */
+/*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 18:19:39 by lmaes             #+#    #+#             */
-/*   Updated: 2024/11/28 18:19:39 by lmaes            ###   ########.fr       */
+/*   Updated: 2025/02/13 02:09:05 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ int	ft_isredirection(char c)
 	return (c == '<' || c == '>');
 }
 
-//counts the number of args in a array
 int	ft_countargs(char **args)
 {
 	int	i;
@@ -32,44 +31,40 @@ int	ft_countargs(char **args)
 		i++;
 	return (i);
 }
+
+static void	init_token(t_tokens *temp, t_tokens *prev, t_msh *msh, int i)
+{
+	temp->name = msh->data->args[i];
+	temp->args = NULL;
+	if (prev == NULL || prev->type == TKN_PIPE)
+		temp->type = get_type(msh, msh->data->args[i], i);
+	else
+		temp->type = get_meta_type(msh, msh->data->args[i], 1);
+	if (temp->type == TKN_PIPE)
+		msh->data->pipes++;
+	temp->count = msh->data->pipes;
+	temp->prev = prev;
+	temp->next = NULL;
+}
+
 void	split_tokens(t_msh *msh, t_tokens **token, t_tokens *prev, int i)
 {
 	t_tokens	*temp;
 
 	if (i < msh->data->argc && msh->data->args[i][0] == '\0')
 		i++;
-	if (i < msh->data->argc)
+	if (i >= msh->data->argc || !msh || !msh->data)
+		return ;
+	*token = malloc(sizeof(t_tokens));
+	if (!*token)
 	{
-		if (!msh || !msh->data)
-			return ;
-		*token = NULL;
-		*token = malloc(sizeof(t_tokens));
-		if (!*token)
-		{
-			g_exit = 1;
-			return ;
-		}
-		temp = *token;
-		temp->name = msh->data->args[i];
-		temp->args = NULL;
-		if (prev == NULL || prev->type == TKN_PIPE)
-			temp->type = get_type(msh, msh->data->args[i], i);
-		else
-			temp->type = get_meta_type(msh, msh->data->args[i], 1);
-		if (temp->type == TKN_PIPE)
-			msh->data->pipes++;
-		temp->count = msh->data->pipes;
-		if (prev != NULL)
-			temp->prev = prev;
-		else
-			temp->prev = NULL;
-		if (temp->type == TKN_HEREDOC)
-		{
-			while (i + 1 < msh->data->argc && msh->data->args[i + 1][0] == ' ')
-				i++;
-		}
-		temp->next = NULL;
-		i++;
-		split_tokens(msh, &temp->next, temp, i);
+		g_exit = 1;
+		return ;
 	}
+	temp = *token;
+	init_token(temp, prev, msh, i);
+	if (temp->type == TKN_HEREDOC)
+		while (i + 1 < msh->data->argc && msh->data->args[i + 1][0] == ' ')
+			i++;
+	split_tokens(msh, &temp->next, temp, i + 1);
 }
